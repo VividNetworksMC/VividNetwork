1b70b631b98e644c8875f65979f6a5a6299c9414
SkylerTyler1337 <tylernichols409@gmail.com>
2014-04-06 17:41:28 -0500

    1.2 added the displaynames to the VanishTorchListener

--- src/net/vividnetwork/listeners/VanishTorchListener.java
+++ src/net/vividnetwork/listeners/VanishTorchListener.java
@@ -24,42 +24,65 @@ public class VanishTorchListener implements Listener{
     	this.plugin = plugin;
      }
 		
+    public enum Items{
+    	magmacream,
+    	slimeball,
+    }
+    
+    public ItemStack getCustName(Items item){
+		ItemStack is = null;
+		ItemMeta im;
+		switch(item){
+		case slimeball:
+			is = new ItemStack(Material.SLIME_BALL);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "Show Players");
+		    is.setItemMeta(im);
+			break;
+		case magmacream:
+			is = new ItemStack(Material.MAGMA_CREAM);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Hide Players");
+			is.setItemMeta(im);
+		default:
+			break;
+		
+		}
+		return is;
+		
+	}
+
      @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event){
-    	if(!(event.getPlayer().hasPermission("vanishtorch.use")))
-    		return;
-    	if(event.getItem() == null || event.getItem().equals(Material.AIR))
-            return;
-    if (event.getPlayer().hasPermission("vanishtorch.use")) {
-        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (event.getItem().getType().equals(Material.MAGMA_CREAM)){
-                Player player = event.getPlayer();
-                for (Player other : plugin.getServer().getOnlinePlayers()) {
-                    player.hidePlayer(other);
-                    player.playSound(player.getLocation(), Sound.ANVIL_LAND, 10, 1);
-                    player.setItemInHand(new ItemStack(Material.SLIME_BALL));
-                    player.sendMessage(ChatColor.GRAY + "Players toggled: "+ChatColor.RED.toString()+ChatColor.BOLD+"Off"+ChatColor.GRAY+"!");
-                    return;
-                }
-                return;
-            }
-            
-            if(event.getItem() == null || event.getItem().equals(Material.AIR))
-              return;
-              if(event.getItem().getType() != Material.SLIME_BALL)
-            	  return;
-            if (event.getItem().getType().equals(Material.SLIME_BALL)){
-                Player player = event.getPlayer();
-                for (Player other : plugin.getServer().getOnlinePlayers()) {
-                    player.showPlayer(other);
-                    player.playSound(player.getLocation(), Sound.ANVIL_BREAK, 10, 1);
-                    player.setItemInHand(new ItemStack(Material.MAGMA_CREAM));
-                    player.sendMessage(ChatColor.GRAY + "Players toggled: "+ChatColor.GREEN.toString()+ChatColor.BOLD+"On"+ChatColor.GRAY+"!");
-                    return;
-                }
-                return;
-            }
-        }
-    }
- }
+    	 Player player = event.getPlayer();
+    	 ItemStack is = player.getItemInHand();
+    	 Action action = event.getAction();
+    	 
+    	 if(!(player.hasPermission("vanishtorch.use")))
+    		 return;
+    	 
+    	 if(is == null || is.getType().equals(Material.AIR))
+    	 return;;
+    	 
+    	 if(player.hasPermission("vanishtorch.use")){
+    		if(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK){
+    		 if(is.getType().equals(Material.MAGMA_CREAM) && is.hasItemMeta()){
+    			 for(Player others : this.plugin.getServer().getOnlinePlayers()){
+    			 player.hidePlayer(others);
+                 player.playSound(player.getLocation(), Sound.ANVIL_LAND, 10, 1);
+                 player.setItemInHand(new ItemStack(getCustName(Items.slimeball)));
+                 player.sendMessage(ChatColor.GRAY + "Players ✈ "+ChatColor.RED.toString()+ChatColor.BOLD+"Disabled");
+    			 }
+    		  }
+    		 if(is.getType().equals(Material.SLIME_BALL) && is.hasItemMeta()){
+              	 for(Player others : this.plugin.getServer().getOnlinePlayers()){
+    	    	     player.hidePlayer(others);
+                     player.playSound(player.getLocation(), Sound.ANVIL_BREAK, 10, 1);
+                     player.setItemInHand(new ItemStack(getCustName(Items.magmacream)));
+                     player.sendMessage(ChatColor.GRAY + "Players ✈ "+ChatColor.GREEN.toString()+ChatColor.BOLD+"Enabled");
+    			 }
+    		  }
+    	    }
+    	 }
+     }
 }
