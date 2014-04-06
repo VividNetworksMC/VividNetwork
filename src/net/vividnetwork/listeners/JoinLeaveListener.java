7717a210041607218921fc4a693f4ccb5c7e72d5
SkylerTyler1337 <tylernichols409@gmail.com>
2014-04-06 18:02:19 -0500

    Update JoinLeaveListener.java

--- src/net/vividnetwork/listeners/JoinLeaveListener.java
+++ src/net/vividnetwork/listeners/JoinLeaveListener.java
@@ -26,6 +26,49 @@ public class JoinLeaveListener implements Listener {
 	
 	
 	
+        public enum Items{
+		Compass,
+		fishingrod,
+		magmacream,
+		slimball,
+	}
+	
+	
+	public ItemStack getCustName(Items item){
+		ItemStack is = null;
+		ItemMeta im;
+		switch(item){
+		case Compass:
+			is = new ItemStack(Material.COMPASS);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.GOLD.toString()+ ChatColor.BOLD + "Server Selector");
+			is.setItemMeta(im);
+			break;
+		case fishingrod:
+			is = new ItemStack(Material.FISHING_ROD);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Grappling Hook");
+			is.setItemMeta(im);
+			break;
+		case slimball:
+			is = new ItemStack(Material.SLIME_BALL);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "Show Players");
+		    is.setItemMeta(im);
+			break;
+		case magmacream:
+			is = new ItemStack(Material.MAGMA_CREAM);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Hide Players");
+			is.setItemMeta(im);
+		default:
+			break;
+		
+		}
+		return is;
+		
+	}
+	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event){
             Player player = event.getPlayer();
@@ -33,9 +76,9 @@ public class JoinLeaveListener implements Listener {
             player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 360000, plugin.getConfig().getInt("JumpStrength")));
             player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 360000, plugin.getConfig().getInt("SpeedStrength")));
             player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 360000, plugin.getConfig().getInt("SpeedStrength")));
-            player.getInventory().setItem(0, new ItemStack(Material.COMPASS));
-            player.getInventory().setItem(2, new ItemStack(Material.BOOK));
-            player.getInventory().setItem(8, new ItemStack(Material.MAGMA_CREAM));
+            player.getInventory().setItem(0, new ItemStack(getCustName(Items.Compass)));
+            player.getInventory().setItem(3, new ItemStack(getCustName(Items.fishingrod)));
+            player.getInventory().setItem(2, new ItemStack(getCustName(Items.magmacream)));
     }
 	
 	
