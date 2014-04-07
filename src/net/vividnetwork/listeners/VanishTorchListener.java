96f0f1c45dff3de07849d0ad4f5e1df16ea893b7
SkylerTyler1337 <tylernichols409@gmail.com>
2014-04-06 20:49:34 -0500

    Update VanishTorchListener.java

--- src/net/vividnetwork/listeners/VanishTorchListener.java
+++ src/net/vividnetwork/listeners/VanishTorchListener.java
@@ -26,72 +26,65 @@ public class VanishTorchListener implements Listener{
      }
    
     
-    public ItemStack getCustName(Items item){
-		ItemStack is = null;
-		ItemMeta im;
-		switch(item){
-		case slimeball:
-			is = new ItemStack(Material.SLIME_BALL);
-			im = is.getItemMeta();
-			im.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "Show Players");
-		    is.setItemMeta(im);
-			break;
-		case magmacream:
-			is = new ItemStack(Material.MAGMA_CREAM);
-			im = is.getItemMeta();
-			im.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Hide Players");
-			is.setItemMeta(im);
-		default:
-			break;
-		
-		}
-		return is;
-		
-	}
 
-     @EventHandler
+    package net.vividnetwork.listeners;
+
+import net.vividnetwork.Items;
+import net.vividnetwork.JoinItems;
+import net.vividnetwork.VividNetwork;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class VanishTorchListener implements Listener{
+    
+    
+    public VividNetwork plugin;
+    
+    public VanishTorchListener(VividNetwork plugin){ 
+    	this.plugin = plugin;
+     }
+   
+ 
+
+    @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event){
     	 Player player = event.getPlayer();
-    	 ItemStack is = player.getItemInHand();
+    	 ItemStack itemstack = player.getItemInHand();
     	 Action action = event.getAction();
     	 
     	 if(!(player.hasPermission("vanishtorch.use")))
     		 return;
     	 
-    	 if(is == null || is.getType().equals(Material.AIR))
+    	 if(itemstack == null || itemstack.getType().equals(Material.AIR))
     	 return;;
     	 
     	 if(player.hasPermission("vanishtorch.use")){
     		if(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK){
-    		 if(is.getType().equals(Material.MAGMA_CREAM) && is.hasItemMeta()){
+    		 if(itemstack.getType().equals(Material.MAGMA_CREAM) && itemstack.hasItemMeta()){
     			 for(Player others : this.plugin.getServer().getOnlinePlayers()){
     			 player.hidePlayer(others);
                  player.playSound(player.getLocation(), Sound.ANVIL_LAND, 10, 1);
-                 player.setItemInHand(new ItemStack(getCustName(Items.slimeball)));
-                 player.sendMessage(ChatColor.GRAY + "Players Toggled: "+ChatColor.RED.toString()+ChatColor.BOLD+"Disabled");
+                 player.setItemInHand(new ItemStack(JoinItems.getCustName(Items.slimeball)));
     			 }
     		  }
     		}
     	if(action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK){
-    		 if(is.getType().equals(Material.SLIME_BALL) && is.hasItemMeta()){
+    		 if(itemstack.getType().equals(Material.SLIME_BALL) && itemstack.hasItemMeta()){
     			 for(Player others : this.plugin.getServer().getOnlinePlayers()){
     				 player.showPlayer(others);
                      player.playSound(player.getLocation(), Sound.ANVIL_BREAK, 10, 1);
-                     player.setItemInHand(new ItemStack(getCustName(Items.magmacream)));
-                     player.sendMessage(ChatColor.GRAY + "Players Toggled: "+ChatColor.GREEN.toString()+ChatColor.BOLD+"Enabled");
+                     player.setItemInHand(new ItemStack(JoinItems.getCustName(Items.magmacream)));;
     		   }
     		 }
     	   }
     	 }
      }
-     
-     
-     @EventHandler
-     public void onPlayerJoin(PlayerJoinEvent event){
-    	 Player player = event.getPlayer();
-    	 
-    	 PlayerInventory pi = player.getInventory();
-    	 pi.setItem(2, getCustName(Items.magmacream));
-     }
 }
-
+     
