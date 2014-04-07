a05d25625d10443b412080bb01cf667760c9f26f
SkylerTyler1337 <tylernichols409@gmail.com>
2014-04-06 20:44:30 -0500

    Create JoinItems.java

---
+++ src/net/vividnetwork/JoinItems.java
@@ -0,0 +1,45 @@
+package net.vividnetwork;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class JoinItems {
+	
+	
+	public static ItemStack getCustName(Items item){
+		ItemStack is = null;
+		ItemMeta im;
+		switch(item){
+		case slimeball:
+			is = new ItemStack(Material.SLIME_BALL);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + "Show Players");
+		    is.setItemMeta(im);
+			break;
+		case magmacream:
+			is = new ItemStack(Material.MAGMA_CREAM);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Hide Players");
+			is.setItemMeta(im);
+			break;
+		case Compass:
+			is = new ItemStack(Material.COMPASS);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.GOLD.toString()+ ChatColor.BOLD + "Server Selector");
+			is.setItemMeta(im);
+			break;
+		case fishingrod:
+			is = new ItemStack(Material.FISHING_ROD);
+			im = is.getItemMeta();
+			im.setDisplayName(ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD + "Grappling Hook");
+			is.setItemMeta(im);
+			break;
+		default:
+			break;
+		 }
+		return is;
+	}
+
+}
