97a318af468641c8f18ec3e8281adc84c9edd031
TheBrownBear <damulye@gmail.com>
2014-04-08 21:44:49 -0700

    Bug Fixes

--- src/net/vividnetwork/StopEffectCommand.java
+++ src/net/vividnetwork/StopEffectCommand.java
@@ -5,32 +5,27 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 public class StopEffectCommand implements CommandExecutor {
+ 
+ public VividNetwork plugin;
+ 
+ public StopEffectCommand(VividNetwork plugin) {
+  this.plugin = plugin;
+ }
 
-	public VividNetwork plugin;
-
-	public StopEffectCommand(VividNetwork plugin)
-	{
-		this.plugin = plugin;
-	}
-
-	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args)
-	{
-		Player player = (Player) sender;
-
-		if(commandLabel.equalsIgnoreCase("stopeffect") || commandLabel.equalsIgnoreCase("se"))
-		{
-			player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST,360000, 0));
-			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,360000, 0));
-			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP,360000, 0));
-			player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL,360000, 0));
-			player.sendMessage(ChatColor.RED + "Potion Effects Have Been Removed!");
-		}
-
-		return false;
-	}
+ @Override
+ public boolean onCommand(CommandSender sender, Command command, String label,
+   String[] args) {
+  if(sender instanceof Player){
+   Player player = (Player) sender;
+   if(label.equalsIgnoreCase("stopeffect")){
+    player.getActivePotionEffects().clear();    
+  }else{
+   sender.sendMessage(ChatColor.RED+"You need to be a player to do this command!");
+   }
+  }
+  return false;
+ }
 
 }
